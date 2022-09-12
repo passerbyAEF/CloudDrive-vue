d7da206a6eeeb04d6bbad1fd6bbc0876c477454a
chen <807477447@qq.com>
2022-09-12 17:10:33 +0800

    登录注册页

--- LoginPage/src/App.vue
+++ LoginPage/src/App.vue
@@ -1,28 +1,15 @@
 <script setup>
+import { ref } from 'vue';
+import loginPage from './components/login.vue'
+import registerPage from './components/register.vue'
+
+let k = ref(true)
+
 </script>
-
 <template>
-  <form action="">
-    <img class="mb-4" src="https://avatars.githubusercontent.com/u/43606074?v=4" width="150" height="150">
-    <h1 class="h3 mb-3 fw-normal">请登录</h1>
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">邮箱</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">密码</label>
-    </div>
-    <div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> 记住我
-      </label>
-    </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">登录</button>
-    <button class="w-100 mt-2 btn btn-lg btn-primary" type="submit">注册</button>
-    <p class="mt-5 mb-3 text-muted">Passerby-Alpha</p>
-  </form>
+  <loginPage @gotoRegister="k=false" v-if="k" />
+  <registerPage @gotoLogin="k=true" v-else/>
 </template>
-
 <style scoped>
+
 </style>
---
+++ LoginPage/src/components/register.vue
@@ -0,0 +1,30 @@
+<script setup>
+const emit = defineEmits(['gotoLogin'])
+
+function gotoLogin() {
+    emit('gotoLogin')
+}
+</script>
+<template>
+  <form action="">
+    <img class="mb-4" src="https://avatars.githubusercontent.com/u/43606074?v=4" width="150" height="150">
+    <h1 class="h3 mb-3 fw-normal">注册</h1>
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">邮箱</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">密码</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">请重复输入密码</label>
+    </div>
+    <button class="w-100 mt-2 btn btn-lg btn-primary" type="submit">注册</button>
+    <button class="w-100 mt-2 btn btn-lg btn-primary" @click="gotoLogin" type="button">返回</button>
+    <p class="mt-5 mb-3 text-muted">Passerby-Alpha</p>
+  </form>
+</template>
+<style scoped>
+</style>
